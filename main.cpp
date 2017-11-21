3035fb8f6e256296dd938af70cb7d41a6ba03ef1
Vivien Blum <vivien.blum@a-018818.ups.u-psud.fr>
2017-11-21 17:10:05 +0100

    Setup getA

--- main.cpp
+++ main.cpp
@@ -82,10 +82,22 @@ float averageMat(Mat matrix) {
 	return sum/(matrix.rows*matrix.cols);
 }
 
+/* Fonction pour récupérer une partie de l'image */
+Mat getSample(Mat image, Point2i origine, int delta = 2) {
+	Mat sample = Mat::zeros(Size(delta*2 + 1, delta*2 + 1), DataType<uchar>::type);
+	for( int x = origine.x - delta, i = 0; x < image.rows && x <= origine.x + delta; x++, i++ ) { 
+		for( int y = origine.y - delta, j = 0; y < image.cols && y <= origine.y + delta; y++, j++) { 
+			sample.at<uchar>(i, j) = image.at<uchar>(x, y);
+		}
+	}
+	return sample;
+}
+
 /* Fonction pour trouver le point qui match le mieux */
 Point2i pointMatch(Mat imageIn1, Mat imageIn2, Point2i pointOriginal, vector<Point2i> vCompared) {
 	// On récupère les voisins proche du points
 	Mat squareOriginal = imageIn1( Rect(pointOriginal.x, pointOriginal.y, DELTA_SQUARE, DELTA_SQUARE) );
+	// Mat squareOriginal = getSample(imageIn1, pointOriginal);
 	// On récupère la moyenne de la matrice 
 	int mu1 = averageMat(squareOriginal);
 
@@ -96,6 +108,7 @@ Point2i pointMatch(Mat imageIn1, Mat imageIn2, Point2i pointOriginal, vector<Poi
 	//On parcourt les points de contours de l'image 2
 	for(unsigned int j = 0; j < vCompared.size(); j++) {
 		Mat squareCompared = imageIn2( Rect(vCompared[j].x, vCompared[j].y, DELTA_SQUARE, DELTA_SQUARE) );
+		// Mat squareCompared = getSample(imageIn2, vCompared[j]);
 		int mu2 = averageMat(squareCompared);
 		int sum = 0;
 		//On les compares
@@ -167,6 +180,36 @@ vector<vector<Point2i> > getRandomMatches(vector<vector<Point2i> > matches, int 
 	return randomMatches;
 }
 
+Mat getA(vector<vector<Point2i> > matches) {
+	Mat A = Mat::zeros(Size(9, matches.size()*2), DataType<uchar>::type);
+
+	// On parcourt les lignes
+	for( int x = 0, i = 0; x < A.rows; x+=2, i++ ) { 
+		// On construit la 1ère ligne
+		A.at<uchar>(x, 0) = matches[i][0].x;	// xn
+		A.at<uchar>(x, 1) = matches[i][0].y;	// yn
+		A.at<uchar>(x, 2) = 1;	// 1
+		A.at<uchar>(x, 3) = 0;	// 0
+		A.at<uchar>(x, 4) = 0;	// 0
+		A.at<uchar>(x, 5) = 0;	// 0
+		A.at<uchar>(x, 6) = -matches[i][1].x*matches[i][0].x;	// -xn'xn
+		A.at<uchar>(x, 7) = -matches[i][1].x*matches[i][0].y;	// -xn'yn
+		A.at<uchar>(x, 8) = -matches[i][1].x;	// -xn'
+
+		// On construit la 2ème ligne
+		A.at<uchar>(x + 1, 0) = 0;	// 0
+		A.at<uchar>(x + 1, 1) = 0;	// 0
+		A.at<uchar>(x + 1, 2) = 0;	// 0
+		A.at<uchar>(x + 1, 3) = matches[i][0].x;	// xn
+		A.at<uchar>(x + 1, 4) = matches[i][0].y;	// yn
+		A.at<uchar>(x + 1, 5) = 1;	// 1
+		A.at<uchar>(x + 1, 6) = -matches[i][1].y*matches[i][0].x;	// -yn'xn
+		A.at<uchar>(x + 1, 7) = -matches[i][1].y*matches[i][0].y;	// -yn'yn
+		A.at<uchar>(x + 1, 8) = -matches[i][1].y;	// -yn'
+	}
+	return A;
+}
+
 int main(int argc, char** argv){
 	
 	Mat imageIn1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
@@ -203,6 +246,19 @@ int main(int argc, char** argv){
 		cout <<  "Right, MY_FAST : " << v2.size() <<  " FAST : " << v2Fast.size() << endl;
 
 		vector<vector<Point2i> > matches = getMatches(imageIn1, imageIn2, v1, v2);
+
+		// cout << getA(getRandomMatches(matches)) << endl;
+		/* TEST pour homography */
+		vector<Point2i> pts_src;
+		vector<Point2i> pts_dst;
+		for(unsigned int i = 0; i < matches.size(); i++) {
+			pts_src.push_back(matches[i][0]);
+			pts_dst.push_back(matches[i][1]);
+		}
+		Mat h = findHomography( pts_src, pts_dst, CV_RANSAC );
+		cout <<  h << endl;
+		// Mat imageOut2;
+		// warpPerspective(imageIn1, imageOut2, h, imageIn2.size());
 
 		// showCorners(imageOut, v1);
 		// showCorners(imageOut, v2, decalage);
